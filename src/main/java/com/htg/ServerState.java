fa177712f20c6535ebdd572c344abce05a572b26
Baizey <lassemoerk.mrk@gmail.com>
2017-04-22 14:50:08 +0200

    gheruigh

--- src/main/java/com/htg/ServerState.java
+++ src/main/java/com/htg/ServerState.java
@@ -10,6 +10,7 @@ import java.util.function.Function;
 @Component
 public class ServerState {
     private Game currentGame = null;
+    private HashMap<String, User> sessionIds = new HashMap<>();
     private HashMap<String, User> users = new HashMap<>();
     /**
      * TODO: maybe add time limit on challenges?
@@ -17,13 +18,19 @@ public class ServerState {
     private long challengeStartTime;
     private long challengeEndTime;
 
-    synchronized boolean addUser( User user ){
+    synchronized boolean addUser( String sessionId, User user ){
         if(users.containsKey(user.getName()))
             return false;
         users.put(user.getName(), user);
+        sessionIds.put(sessionId, user);
         return true;
     }
 
+    User getUser (String sessionId) {
+        if(sessionIds.containsKey(sessionId))
+            return sessionIds.get(sessionId);
+        return null;
+    }
 
     synchronized public Game getCurrentGame () {
         return currentGame;
