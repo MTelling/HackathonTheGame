2e5dfbf6317b40004d8293caeaac7a9cd6af07b9
Tolini <Toli.TobiasLindstrom@gmail.com>
2017-04-22 17:17:48 +0200

    Added 30 second timeout and the testing framework now terminates if it encounters an error

--- Compiler/src/Launcher.java
+++ Compiler/src/Launcher.java
@@ -8,6 +8,7 @@ import java.io.FileReader;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Paths;
+import java.util.concurrent.*;
 
 /**
  * Created by Tobias on 4/22/2017.
@@ -33,42 +34,91 @@ public class Launcher {
             challenge = gson.fromJson(new BufferedReader(new FileReader(root.getAbsolutePath() + "/Testing/Tests/" + args[0] + ".json")), Challenge.class);
         } catch (FileNotFoundException e) {
             result.compilerErrors.add(e.getMessage());
+            System.out.println(gson.toJson(result, Result.class));
+            System.exit(0);
         }
 
         if(challenge != null) {
+            Object prg = null;
+            java.lang.reflect.Method method = null;
             try {
                 URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{root.toURI().toURL()});
                 Class<?> aClass = Class.forName("Testing.Program", true, classLoader);
-                Object prg = aClass.newInstance();
+                prg = aClass.newInstance();
 
-                java.lang.reflect.Method method = aClass.getMethod("run", String[].class);
-                for(int i = 0; i < challenge.getTests().size(); i++) {
-                    Test test = challenge.getTests().get(i);
-                    Object[] actual = (Object[]) method.invoke(prg, new Object[]{test.getArguments()});
-                    if(actual.length != test.getExpectedReturns().length) {
-                        result.errors.add("Error in test " + (i+1) + ": Number of returned variables is wrong");
-                    } else {
-                        for(int k = 0; k < test.getExpectedReturns().length; k++) {
-                            Object expected = test.getExpectedReturns()[k];
-                            if(expected.toString().indexOf(".0") == expected.toString().length() - 2) {
-                                expected = Integer.parseInt(expected.toString().replace(".0", ""));
-                            }
-                            if(!actual[k].equals(expected)) {
-                                result.errors.add("Error in test " + (i+1) + ": " + actual[k] + " should be " + expected);
-                            } else {
-                                result.passedTests += 1;
-                            }
-                        }
-                    }
-                }
-                if(result.passedTests == challenge.getTests().size()) {
-                    result.success = true;
-                }
+                method = aClass.getMethod("run", String[].class);
             } catch (Exception e) {
                 result.compilerErrors.add(e.getMessage());
+                System.out.println(gson.toJson(result, Result.class));
+                System.exit(0);
+            }
+            for (int i = 0; i < challenge.getTests().size(); i++) {
+                Test test = challenge.getTests().get(i);
+                ExecutorService executor = Executors.newSingleThreadExecutor();
+                Future<String> future = executor.submit(new CodeChecker(prg, method, result, test, i));
+                try {
+                    if(future.get(30, TimeUnit.SECONDS).equals("error")) {
+                        System.out.println(gson.toJson(result, Result.class));
+                        System.exit(0);
+                    }
+                } catch (TimeoutException ex) {
+                    future.cancel(true);
+                    result.compilerErrors.add("Error in test " + (i+1) + ": Program took too long to run!");
+                    System.out.println(gson.toJson(result, Result.class));
+                    System.exit(0);
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (result.passedTests == challenge.getTests().size()) {
+                result.success = true;
             }
         }
         System.out.println(gson.toJson(result, Result.class));
     }
+}
 
+class CodeChecker implements Callable<String> {
+    Object prg;
+    java.lang.reflect.Method method;
+    Result result;
+    Test test;
+    int round;
+
+    public CodeChecker(Object program, java.lang.reflect.Method method, Result result, Test test, int round) {
+        this.prg = program;
+        this.method = method;
+        this.result = result;
+        this.test = test;
+        this.round = round;
+    }
+
+    @Override
+    public String call() throws Exception {
+        String check = "";
+        Object[] actual = new Object[0];
+        try {
+            actual = (Object[]) method.invoke(prg, new Object[]{test.getArguments()});
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (actual.length != test.getExpectedReturns().length) {
+            result.errors.add("Error in test " + (round + 1) + ": Number of returned variables is wrong");
+            check = "error";
+        } else {
+            for (int k = 0; k < test.getExpectedReturns().length; k++) {
+                Object expected = test.getExpectedReturns()[k];
+                if (expected.toString().indexOf(".0") == expected.toString().length() - 2) {
+                    expected = Integer.parseInt(expected.toString().replace(".0", ""));
+                }
+                if (!actual[k].equals(expected)) {
+                    result.errors.add("Error in test " + (round + 1) + ": " + actual[k] + " should be " + expected);
+                    check="error";
+                } else {
+                    result.passedTests += 1;
+                }
+            }
+        }
+        return check;
+    }
 }
