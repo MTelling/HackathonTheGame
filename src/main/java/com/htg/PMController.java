1516d1da0ad31c0eab38a814233284005d9b55b2
Baizey <lassemoerk.mrk@gmail.com>
2017-04-22 17:48:29 +0200

    pm workssss

--- src/main/java/com/htg/PMController.java
+++ src/main/java/com/htg/PMController.java
@@ -1,6 +1,7 @@
 package com.htg;
 
 import org.apache.catalina.Server;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -10,7 +11,12 @@ import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 
 @Controller
@@ -31,20 +37,46 @@ public class PMController {
         System.out.println("In the pmcontroller");
 
 
-        String code = pmRequest.getCode();
-        // TODO: compile code, do magic, test results
+        String  code = pmRequest.getCode(),
+                fileName = sessionID + ".java",
+                path = Paths.get("").toAbsolutePath().toString() + "\\Testing\\";
 
+        // Write code to java file
+        try(  PrintWriter out = new PrintWriter( "Testing/" + fileName ) ){ out.println( code ); }
+
+        // Compile code to class file
         ProcessBuilder builder = new ProcessBuilder();
-        builder.command("");
+        builder.command( "javac", path + fileName );
         Process proc = builder.start();
-        BufferedReader output = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        BufferedReader output = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
         proc.waitFor();
 
+        // If code fails compiling, return some sort of error for that
+        if ( output.lines().toArray().length > 0 ) {
+            System.out.println("ERROR COMPILING");
+            return new PMResponse("ERROR COMPILING");
+        }
+
+        // Code could compile, run tests and return results
+        builder.command("jar", "");
+        proc = builder.start();
+        output = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        proc.waitFor();
+
+        // Debug log
+        System.out.println("Process completed");
+        System.out.println(Arrays.toString(output.lines().toArray()));
+
+        // Build result
         StringBuilder result = new StringBuilder();
-        result.append(output.lines());
+        for(Object s : output.lines().toArray())
+            result.append((String) s);
+
+        // Check if all tests are completed
         boolean hasWon = result.toString().split(",")[0].split(" ")[1].equals("true");
-        if(hasWon)
+        if ( hasWon )
             announceWin(state.getUser(sessionID).getName());
+
         return new PMResponse(result.toString());
     }
 
