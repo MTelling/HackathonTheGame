c2e28691fe7f3cefe40f8c47e92c7b25446e5e9d
tony <duma0311@gmail.com>
2017-04-23 08:19:43 +0200

    Add reset

--- src/main/resources/static/js/components/Login.jsx
+++ src/main/resources/static/js/components/Login.jsx
@@ -6,13 +6,17 @@ import LoginForm from './LoginForm';
 export default class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
+    this.state = {
+      exists: false,
+      value: "",
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
     this.setState({
+      exists: false,
       value: e.target.value,
     });
   };
@@ -25,16 +29,17 @@ export default class Login extends Component {
 
   render() {
     return (
-      <div style={{padding: "20% 0"}}>
+      <div style={{padding: "15% 0"}}>
         <Paper className="loginContainer" zDepth={2} >
-          <div id="loginErrorContainer" style={{display:"none"}}>
-            <p>User name is already in use. Try another!</p>
+          <div id="loginErrorContainer" style={{display:!this.state.exists?"none":"inline-block"}}>
+            <span style={{color:"#EF5350"}}>{this.state.value}</span> is already in use. Try another!
           </div>
           <LoginForm
             value={this.state.value}
             onChange={this.handleChange}
             onSubmit={this.handleSubmit}/>
         </Paper>
+        <div className="loginFooter">HackthonTheGame</div>
       </div>
     );
   }
